57550cbef3e5b851f1c452db96f2d30837052ce2
nascjoao <oi@joaonasc.dev>
2020-12-22 20:25:13 -0300

    Set folders to be used by the server and configure nunjucks

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const server = express()
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjucks.configure('src/app/views', {
+    express: server,
+    noCache: true
+})
 
 server.listen(5000, () => console.log('Server is running.'))
