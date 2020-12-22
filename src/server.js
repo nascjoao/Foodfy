9aac05f794b6aa64fe44c87cda19384ae9cc329a
nascjoao <oi@joaonasc.dev>
2020-12-22 20:50:06 -0300

    Import routes and set server to use them

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require('./routes')
 
 const server = express()
 
+server.use(routes)
 server.use(express.static('public'))
 
 server.set('view engine', 'njk')
