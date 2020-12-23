c92e0512b106412e9828115f9f445ee6c4b0198e
nascjoao <oi@joaonasc.dev>
2020-12-23 11:25:54 -0300

    Create route for about page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => { return res.render('home/index') })
+routes.get('/sobre', (req, res) => { return res.render('sobre/index') })
 
 module.exports = routes
