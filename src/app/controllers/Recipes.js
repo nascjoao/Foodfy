9f89eabd00b15f9ee215364083f60c11efef5d70
nascjoao <oi@joaonasc.dev>
2020-12-23 13:21:18 -0300

    Create method to show recipe singlepage

--- src/app/controllers/Recipes.js
+++ src/app/controllers/Recipes.js
@@ -3,5 +3,13 @@ const recipes = require('../data')
 module.exports = {
     index(req, res) {
         return res.render('recipes/index', { recipes })
+    },
+
+    show(req, res) {
+        const recipeIndex = req.params.index;
+
+        const recipe = recipes[recipeIndex]
+
+        return res.render('recipes/show', { recipe })
     }
 }
