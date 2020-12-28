3b6b75c9febbe0c472a958f16ee7633cf36cf39c
nascjoao <oi@joaonasc.dev>
2020-12-28 08:47:57 -0300

    Add function to admin forms to be able to create or remove several fields

--- public/scripts/recipes.js
+++ public/scripts/recipes.js
@@ -23,3 +23,41 @@ if (sections) {
         })
     })
 }
+
+function addInput(containerName) {
+    const container = document.querySelector(`#${containerName}`)
+    const fieldContainer = container.children
+    const newField = fieldContainer[fieldContainer.length - 2].cloneNode(true)
+
+    if (newField.children[0].value == '') return false
+
+    const remove = document.createElement('i')
+    remove.append('remove_circle_outline')
+    remove.classList.add('remove-input', 'material-icons')
+    
+
+    newField.children[0].value = ''
+    if (newField.children[1]) newField.children[1].remove()
+    newField.appendChild(remove)
+    fieldContainer[fieldContainer.length - 2].insertAdjacentElement('afterend', newField)
+}
+
+const addInputs = document.querySelectorAll('.add-input')
+if (addInputs) addInputs.forEach(input => {
+    const containerName = input.parentNode.id
+
+    input.addEventListener('click', () => {
+        addInput(containerName)
+
+        const item = input.parentNode.children[input.parentNode.children.length - 2]
+        const remove = item.children[1]
+        remove.addEventListener('click', () => {
+            item.style.opacity = 0
+            item.style.height = 0
+            setTimeout(() => {
+                item.remove()
+            }, 400)
+        })
+
+    })
+})
