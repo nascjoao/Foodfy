1ec749626081be1255a8f5a20b84e7c37bcc4092
nascjoao <oi@joaonasc.dev>
2021-01-06 20:20:52 -0300

    Set routes for receitas and chefs at admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const routes = express.Router()
+const multer = require('../app/middlewares/multer')
 
-const AdminController = require('../app/controllers/Admin')
+const RecipesController = require('../app/controllers/AdminRecipes')
+const ChefsController = require('../app/controllers/AdminChefs')
 
-routes.get('/receitas', AdminController.index)
-routes.get('/receitas/criar', AdminController.create)
-routes.get('/receitas/:id', AdminController.show)
-routes.get('/receitas/:id/editar', AdminController.edit)
+routes.get('/receitas', RecipesController.index)
+routes.get('/receitas/criar', RecipesController.create)
+routes.get('/receitas/:id', RecipesController.show)
+routes.get('/receitas/:id/editar', RecipesController.edit)
 
-// routes.post('/receitas', AdminController.post)
-// routes.put('/receitas', AdminController.put)
-// routes.delete('/receitas', AdminController.delete)
+routes.post('/receitas', multer.array('photos', 5), RecipesController.post)
+routes.put('/receitas', multer.array('photos', 5), RecipesController.put)
+routes.delete('/receitas', RecipesController.delete)
+
+routes.get('/chefs', ChefsController.index)
+routes.get('/chefs/criar', ChefsController.create)
+routes.get('/chefs/:id', ChefsController.show)
+routes.get('/chefs/:id/editar', ChefsController.edit)
+
+routes.post('/chefs', multer.array('photos', 1), ChefsController.post)
+routes.put('/chefs', multer.array('photos', 1), ChefsController.put)
+routes.delete('/chefs', ChefsController.delete)
 
 module.exports = routes
