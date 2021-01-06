a9aec779b081f5675fac67247af8aa1372e22bae
nascjoao <oi@joaonasc.dev>
2021-01-06 20:18:54 -0300

    Configure multer

---
+++ src/app/middlewares/multer.js
@@ -0,0 +1,25 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now().toString()}-${file.originalname}`)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    const isAccept = ['image/png', 'image/jpg', 'image/jpeg', 'image/webp', 'image/bmp']
+    .find(acceptedFormat => acceptedFormat == file.mimetype)
+
+    if (isAccept) {
+        return cb(null, true)
+    } 
+    
+    return cb(null, false)
+}
+
+module.exports = multer({
+    storage, fileFilter
+})
