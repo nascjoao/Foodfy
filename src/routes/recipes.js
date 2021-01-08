a3c95220dfc079eb8635de438e4863336e805fca
nascjoao <oi@joaonasc.dev>
2021-01-07 21:27:04 -0300

    Change param index to id

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -4,6 +4,6 @@ const routes = express.Router()
 const RecipesController = require('../app/controllers/Recipes')
 
 routes.get('/', RecipesController.index)
-routes.get('/:index', RecipesController.show)
+routes.get('/:id', RecipesController.show)
 
 module.exports = routes
