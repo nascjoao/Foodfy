fa2d78a2445af98d08789882c26179d40c2080bd
nascjoao <oi@joaonasc.dev>
2021-01-07 21:27:31 -0300

    Include chefs routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const recipesRoutes = require('./recipes')
+const chefsRoutes = require('./chefs')
 const adminRoutes = require('./admin')
 
 const HomeController = require('../app/controllers/Home')
@@ -9,6 +10,7 @@ routes.get('/', HomeController.index)
 routes.get('/sobre', (req, res) => { return res.render('sobre/index') })
 
 routes.use('/receitas', recipesRoutes)
+routes.use('/chefs', chefsRoutes)
 routes.use('/admin', adminRoutes)
 
 module.exports = routes
