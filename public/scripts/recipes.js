0c46267ae1c7624fa27a89a222612f54b3f90166
nascjoao <oi@joaonasc.dev>
2021-01-07 21:34:06 -0300

    Receive toggles feature from admin-recipes.js

---
+++ public/scripts/recipes.js
@@ -0,0 +1,25 @@
+const sections = document.querySelectorAll('main:not(#admin) .recipe-show > section')
+if (sections) {
+    sections.forEach(section => {
+        // TOGGLE BUTTON
+        const toggle = document.createElement('button')
+        toggle.append('esconder')
+        toggle.classList.add('toggle')
+
+        // SECTION
+        const sectionContent = section.children[1]
+        const sectionTitle = section.children[0]
+        sectionTitle.appendChild(toggle)
+        const sectionToggle = sectionTitle.children[1]
+
+        sectionToggle.addEventListener('click', () => {
+            if (sectionToggle.innerHTML == 'esconder') {
+                sectionToggle.innerHTML = 'mostrar'
+                sectionContent.classList.add('hidden')
+            } else {
+                sectionToggle.innerHTML = 'esconder'
+                sectionContent.classList.remove('hidden')
+            }
+        })
+    })
+}
