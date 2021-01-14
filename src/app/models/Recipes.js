4f1d6bbb59f31f84c94c20d5a0a9001b48e9eeda
nascjoao <oi@joaonasc.dev>
2021-01-14 14:57:42 -0300

    Implement search feature on listRecipes

--- src/app/models/Recipes.js
+++ src/app/models/Recipes.js
@@ -52,17 +52,28 @@ module.exports = {
         return db.query(`DELETE FROM recipes WHERE id = $1`, [id])
     },
 
-    listRecipes() {
-        return db.query(`
-        SELECT recipes.*, 
-        recipe_files.file_id AS file_id, 
-        files.path AS image_path,
-        chefs.name AS chef_name 
-        FROM recipes
-        LEFT JOIN recipe_files ON (recipes.id = recipe_files.recipe_id)
-        LEFT JOIN files ON (files.id = recipe_files.file_id)
-        LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-         `)
+    listRecipes(search) {
+        let query = '',
+            searchQuery =''
+        
+        if (search) {
+            searchQuery = `
+                WHERE recipes.title ILIKE '%${search}%'
+            `
+        }
+
+        query = `
+            SELECT recipes.*, 
+            recipe_files.file_id AS file_id, 
+            files.path AS image_path,
+            chefs.name AS chef_name 
+            FROM recipes
+            LEFT JOIN recipe_files ON (recipes.id = recipe_files.recipe_id)
+            LEFT JOIN files ON (files.id = recipe_files.file_id)
+            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+            ${searchQuery}
+            `
+        return db.query(query)
     },
 
     find(id) {
