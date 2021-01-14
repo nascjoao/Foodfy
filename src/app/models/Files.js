01942fa954a5160eb7bab7f03fd29672439f371b
nascjoao <oi@joaonasc.dev>
2021-01-14 19:52:54 -0300

    Create Files.update()

--- src/app/models/Files.js
+++ src/app/models/Files.js
@@ -23,6 +23,28 @@ module.exports = {
         return db.query(query, values)
     },
 
+    async update(file, fileId) {
+        const { filename, path } = file
+
+        const result = await db.query(`SELECT * FROM files WHERE id = $1`, [fileId])
+        const oldFile = result.rows[0]
+        fs.unlinkSync(oldFile.path)
+
+        const query = `
+        UPDATE files SET 
+            name = $1, 
+            path = $2 
+        WHERE id = $3`
+
+        const values = [
+            filename,
+            path,
+            fileId
+        ]
+
+        return db.query(query, values)
+    },
+
     async delete(id) {
         const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
         const file = result.rows[0]
